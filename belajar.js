19b7eb760c5ba23b5a77ce999559fa510204c653
FaiqotulJannah0412 <faiqotuljannah883@gmail.com>
2025-09-10 15:00:51 +0700

    belajar.js

--- belajar.js
+++ belajar.js
@@ -1,4 +1,17 @@
 alert("Thank you. ")
 prompt("What should I do?");
 confirm("I'm gonna do something, okay");
-parseInt 
+Date ("7 july 2007")
+
+parseInt("okein aja")
+setTimeout(Faii,2500)
+
+function foo(){
+alert("Our function just ran!");
+} foo(); a
+
+function alertArraySize(arr) {
+aleret(arr.lenght);
+}
+
+var test = [1,2,3,4,5]
